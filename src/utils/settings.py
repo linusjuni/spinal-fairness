9975cf47b5bbb0d0598f108c586b2be24e3652b6
linusjuni <linus@juni.dk>
2026-02-09 15:49:14 +0100

    adds paths to settings

--- src/utils/settings.py
+++ src/utils/settings.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import Field
 from dotenv import load_dotenv
@@ -15,6 +17,34 @@ class Settings(BaseSettings):
 
     # General settings
     RANDOM_SEED: int = Field(default=42)
+
+    # Paths
+    DATA_DIR: Path = Path("/work3/s225224/data/cspineseg")
+    OUTPUT_DIR: Path = Path("outputs")
+
+    @property
+    def raw_dir(self) -> Path:
+        return self.DATA_DIR / "raw"
+
+    @property
+    def extracted_dir(self) -> Path:
+        return self.DATA_DIR / "extracted"
+
+    @property
+    def annotation_dir(self) -> Path:
+        return self.extracted_dir / "annotation"
+
+    @property
+    def segmentation_dir(self) -> Path:
+        return self.extracted_dir / "segmentation"
+
+    @property
+    def structured_dir(self) -> Path:
+        return self.extracted_dir / "structured"
+
+    @property
+    def processed_dir(self) -> Path:
+        return self.DATA_DIR / "processed"
 
 
 settings = Settings()
